21a29f23089b00496fc340f6bfae002cb40225e9
mydreamhorse <mydreamhorse@gmail.com>
2018-12-24 20:36:46 +0800

    modify build.bat for robotframework

--- robotframework/build.bat
+++ robotframework/build.bat
@@ -1,5 +1,4 @@
-docker build -t robotframework --build-arg HTTPS_PROXY=http://10.88.33.116:31288 .
-docker tag robotframework ged-docker-l.production-cn.artifactory.bshg.com/common/robotframework:windows-2.7.15
+docker build -t robertma/robotframework .
 rem you should replace username and password as your own. make sure you have the write permission. 
-docker login ged-docker-l.production-cn.artifactory.bshg.com 
-docker push ged-docker-l.production-cn.artifactory.bshg.com/common/robotframework:windows-2.7.15
+docker login 
+docker push robertma/robotframework
